80b33c230b11143c8ab9f2048463dbb1a3d61c2a
michael <michaelsanchez8815@gmail.com>
2023-11-16 09:47:00 -0600

    adjust user to have avatar property

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -51,3 +51,12 @@ export const extractUsername = (email: string): string | false => {
     return false;
   }
 };
+
+export const getRandomHexColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
